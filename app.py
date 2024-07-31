68a31b16de34187b893bea1fded3eb92d87f5a2f
Cristopher Delgado <delgadocristopher524@gmail.com>
2024-07-31 13:52:23 -0700

    made entire portfolio

--- app.py
+++ app.py
@@ -23,12 +23,44 @@ My experience in biomedical engineering equips me with a unique perspective and 
 """
 EMAIL = "cristopher.d.delgado@gmail.com"
 SOCIAL_MEDIA = {
-    "LinkedIn": "",
-    "GitHub": "", 
+    "LinkedIn": "https://www.linkedin.com/in/cristopher-d-delgado",
+    "GitHub": "https://github.com/cristopher-d-delgado", 
 }
 PROJECTS = {
-    ""
+    "🧠 Brain Lesion Classification": {
+        "link": "https://github.com/cristopher-d-delgado/brain_tumor_classification",
+        "description": [
+            "Implemented a convolutional neural network to classify brain lesions from MRI images by developing a deep learning convolutional neural network utilizing Tensorflow Keras.",
+            "The best model achieved 99% specificity and 99% sensitivity with good generalization to unknown data.",
+            "Merged various datasets to create diverse training images for model training.",
+            "Enhanced learning capability of the model through the use of data augmentation to combat the imbalanced learning dataset.",
+            "Created web application actionable insight using LIME to represent concerning areas of the MRI image slice."
+        ]
+    },
+    "🫁 Pneumonia Detection": {
+        "link": "https://github.com/cristopher-d-delgado/image_classification_pneumonia",
+        "description": [
+            "Developed a deep learning model to detect pneumonia from chest X-ray images.",
+            "Enhanced learning capability of the model through the use of data augmentation to combat the imbalanced learning dataset.",
+            "Conducted hyperparameter tuning to optimize the model's accuracy and reduce overfitting.",
+            "The best model achieved 88% specificity and 94% sensitivity with good generalization to unknown data",
+            "Provided actionable insight into the hypothetical scenario by recommending model implementation into existing software incorporated in medical devices"
+        ]
+    },
+    "🫀 Heart Disease Prediction": {
+        "link": "https://github.com/cristopher-d-delgado/heart_failure",
+        "description": [
+            "Created a logistic regression model to predict the likelihood of heart disease.",
+            "Performed feature engineering to select the most relevant clinical parameters.",
+            "Evaluated the model using precision, recall, and ROC-AUC metrics.",
+            "Built models iteratively to evaluate their baselines in comparison to their optimized versions with hyperparameter tuning.",
+            "Interpreted the model coefficients to determine the most influential factors for heart disease.",
+            "The best model achieved a specificity of 86% and a sensitivity of 87% after hyperparameter tuning.",
+            "Attempted logistic strategies including logistic regression, random forests, gradient boost, XGBoost, AdaBoost, and K-Nearest Neighbors in order to determine the best-performing logistic classifier."
+        ]
+    }
 }
+
 
 # Define page configuration 
 st.set_page_config(page_title=PAGE_TITLE, page_icon=PAGE_ICON)
@@ -61,4 +93,75 @@ with col2:
         mime="application/octet-stream"
     )
     st.write(":e-mail:", EMAIL)
-    
+
+# --- SOCIAL LINKS ---
+st.write("#")
+cols = st.columns(len(SOCIAL_MEDIA))
+
+for index, (platform, link) in enumerate(SOCIAL_MEDIA.items()):
+    cols[index].write(f"[{platform}]({link})")
+
+# --- EXPERIENCE AND QUALIFICTIONS ---
+st.write('#')
+st.subheader("Experience & Qualifications")
+st.write(
+    """
+    - Something
+    """
+)
+
+# --- SKILLS ---
+st.write("#")
+st.subheader("Hard Skills")
+st.write(
+    """
+    - 🧑‍💻Programming: Python (Scikit-learn, Pandas, Tensorflow, SciPy)
+    - :bar_chart: Data Visualization: Matplotlib, Seaborn, Streamlit
+    - 📈 Modeling: Logistic Regression, Linear Regression, Decision Trees, Deep Neural Networks
+    - 🏢 Databases: Sqlite3
+    """
+)
+
+# --- Work History ---
+st.write('#')
+st.subheader("Work History")
+st.write("---")
+
+# --- JOB 1
+st.write("🧬️", "**Intern - Bioinformatics Data Analyst | Orlucent**")
+st.write("07/2023 - Present")
+st.write(
+    """
+    - Conducted comprehensive data analysis, developed machine learning models, generated detailed reports, and provided
+    actionable insights to support informed decision-making
+    - Played a key role in data understanding and data-driven solutions by drawing insights from machine learning models and
+    making suggestions and evaluations for lesion assessment purposes
+    - Develop a deep understanding of startup industry dynamics and challenges while working on a confidential project
+    - Collaborate with cross-functional teams to meet project objectives
+    """
+)
+
+# --- JOB 2
+st.write("#")
+st.write("🧫️", "**Yun Wang Lab - Lab Member | San Jose State University**")
+st.write("01/2022 - 12/2023")
+st.write(
+    """
+    - Collaborated with an interdisciplinary team to design a biosensor prototype with a microfluidic lab on chip technology in
+    conjunction with Quantum Dots resulting in successful trials detection of Botulinum Neurotoxin Serotype A.
+    - Conducted extensive testing of Quantum Dots for their stability and sensitivity for Botulinum Neurotoxin Detection Serotype A
+    resulting in statistical evidence of the biosensors functionality.
+    - Performed Reconstitution of Peptide in 10% Dimethyl Sulfoxide & Botulinum Neurotoxin in HEPES buffer required to perform
+    biosensor testing.
+    """
+)
+
+# --- Projects & Accomplishments ---
+st.write("#")
+st.subheader("Projects & Accomplishments")
+st.write("---")
+
+for project, details in PROJECTS.items():
+    st.write(f"[{project}]({details['link']})")
+    for desc in details['description']:
+        st.write(f"- {desc}")
