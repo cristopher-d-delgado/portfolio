e9e86a2f91b1545a38d7b003923da756e536bf4b
Cristopher Delgado <delgadocristopher524@gmail.com>
2024-07-30 17:27:19 -0700

    defined path settings

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
+from pathlib import Path
+
 import streamlit as st
 from PIL import Image
 
-# Define general information 
+# --- PATH SETTINGS ---
+current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
+css_file = current_dir / "styles" / "main.css"
+resume_file = current_dir / "assests" / "resume.pdf"
+profile_pic = current_dir / "assests" / "profile_pic.png"
+
+# --- Define general information ---
 PAGE_TITLE = ""
 PAGE_ICON = ":wave:"
 NAME = "Cristopher Delgado"
