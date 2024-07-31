cbd21900391ec4504d123589a8fba8d20f09b546
Cristopher Delgado <delgadocristopher524@gmail.com>
2024-07-30 17:15:27 -0700

    made outline

--- app.py
+++ app.py
@@ -1,3 +1,31 @@
 import streamlit as st
-import Pillow as pil
+from PIL import Image
+
+# Define general information 
+
+
+# Header 
+st.write(
+    """
+    # Cristopher Delgado
+    #### *Resume* 
+    """
+)
+
+# Summary section 
+st.markdown('## Summary')
+st.info(
+    """
+    something 
+    """
+)
+
+# Work Experience Section 
+st.markdown('## Work Experience')
+
+# Technical Projects Section 
+st.markdown('## Projects')
+
+# Education Section 
+st.markdown('## Education')
 
